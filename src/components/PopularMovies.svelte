6e094671ae3ba4c12562100cd585424011fee11a
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-08-01 21:04:06 -0400

    fetch themoviedb api

---
+++ src/components/PopularMovies.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let popular
+    
+</script>
+
+<h3>Popular Movies</h3>
+<div class="popular-movies">
+    <div class="movie"></div>
+    {#each popular as movie}
+        <h1>{movie}</h1>
+    {/each}
+</div>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({ fetch }){
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9640f699ceb8be11e7f1bc51d66a7c64&language=en-US&page=1`);
+        const data = await res.json();
+        if(res.ok){
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+</script>
+
+<script>
+    import PopularMovies from '../components/PopularMovies.svelte'
+    export let popular;
+</script>
+
+<main>
+    <PopularMovies {popular}/>
+</main>
